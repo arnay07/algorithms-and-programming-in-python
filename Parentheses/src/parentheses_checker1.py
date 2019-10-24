4afb9c10145e1be25ed40bb4afda7a2a738dd703
Kaderi Arnaud <kaderi@a10p9.fil.univ-lille1.fr>
2019-10-24 11:57:55 +0200

    ajout du module parentheses

---
+++ Parentheses/src/parentheses_checker1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+:mod:`parentheses_checker1` module
+
+:author:Arnaud Kaderi, Elhadj Ibrahima BAH 
+         
+
+:date: 2019, october
+:last revision: 2019, october
+
+"""
+
+
+from stack import *
+import sys
+
+parentheses_dict = {'(':')','[':']','{':'}'}
+
+
+def parentheses_checker():
+    """
+    Checks if the program in the file is well parenthesed
+    
+    """
+
+    global parentheses_dict
+    
+    assert len(sys.argv)==2,'the number of files to check must be one'
+
+    file = sys.argv[1]
+
+    line_stack = Stack()
+    
+    open_file = open(file, 'r')
+
+    line_read = open_file.readline()
+
+    bad_parentheses = False
+
+    while(line_read != '' and not bad_parentheses):
+        i = 0
+        while(i <len(line_read) and not bad_parentheses):
+            if line_read[i] in {'(',')','[',']','{','}'}:
+                if line_read[i] in parentheses_dict.keys():
+                    line_stack.push(line_read[i])
+                elif not line_stack.is_empty() and parentheses_dict[line_stack.top()] == line_read[i]:
+                    line_stack.pop()
+                else:
+                    bad_parentheses = True
+            i+=1
+        line_read = open_file.readline()
+    open_file.close()
+    if bad_parentheses or not line_stack.is_empty():
+        print('Bad parenthesed')
+    else:
+        print('Well parenthesed')
+
+
+
+
+
+
+    
+if __name__=='__main__':
+    parentheses_checker()
+
+
+        
+
